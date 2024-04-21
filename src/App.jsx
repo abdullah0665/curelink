070af45059bb0f5e6db3541fdcd507cc88ddab51
abdullah0665 <shoaib.afzal2002@gmail.com>
2024-04-21 21:54:45 +0500

    routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import React from "react"
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from "./home"
+import About from "./about"
 
 function App() {
-
   return (
     <>
-      hello
+      <Routes>
+        <Route path="/" element={< Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </>
   )
 }
-
 export default App
